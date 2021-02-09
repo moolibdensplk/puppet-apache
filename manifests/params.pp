dde938c470eef85c1293aa48cac4e38345e87f2c
root <root@puppet.example.com>
2021-02-09 09:59:36 +0000

    adding params.pp to handle OS naming differences

---
+++ manifests/params.pp
@@ -0,0 +1,15 @@
+# @summary s
+#
+#  OS related variables
+#
+class apache::params {
+ $install_ensure = 'present'
+ case $::osfamily {
+  'RedHat': {
+    $install_name = 'httpd'
+  }
+  'Debian' {
+    $install_name = 'apache2'
+  }
+ }
+}
