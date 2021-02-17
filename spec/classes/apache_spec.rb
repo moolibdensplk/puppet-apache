1df386dcadc4e92e3009f3df43d2b98b2e19c798
root <root@puppet.example.com>
2021-02-17 14:41:16 +0000

    further unit tests..

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -7,11 +7,25 @@ describe 'apache' do
     context "on #{os}" do
       let(:facts) { os_facts }
 
+      # in case using params.pp file
+      #let :params do {
+      # 	install_name: 'apache2',
+      #  install_ensure: 'present',
+      #} end
 
       it { is_expected.to contain_class('apache::install') }
       it { is_expected.to contain_class('apache::config') }
       it { is_expected.to contain_class('apache::service') }
       it { is_expected.to compile }
+
+      case os_facts[:osfamily]
+      when 'Debian'
+         it { is_expected.to contain_package('apache2').with(ensure: 'present') }
+         it { is_expected.to contain_service('apache2').with(ensure: 'running', enable: true, hasrestart: true) }
+      when 'RedHat'
+         it { is_expected.to contain_package('httpd').with(ensure: 'present') }
+         it { is_expected.to contain_service('httpd').with(ensure: 'running', enable: true, hasrestart: true) }
+      end	      
     end
   end
 end
